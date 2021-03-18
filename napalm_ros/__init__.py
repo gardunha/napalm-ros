c10961b82d93f56e4c61f26cc56c2c4d73cbe07e
Adam Charnock <adam@adamcharnock.com>
2021-03-18 17:11:50 +0000

    Adding plugin metadata as required by Netbox

--- napalm_ros/__init__.py
+++ napalm_ros/__init__.py
@@ -12,3 +12,23 @@ except pkg_resources.DistributionNotFound:
     __version__ = "Not installed"
 
 __all__ = ('ROSDriver', )
+
+# Define the Netbox plugin metadata if Netbox is installed
+try:
+    from extras.plugins import PluginConfig
+except ImportError:
+    pass
+else:
+    class NapalmRosConfig(PluginConfig):
+        name = 'napalm_ros'
+        verbose_name = 'NAPALM RouterOS'
+        description = 'NAPALM Driver for RouterOS'
+        version = '1.0.1'
+        author = 'Łukasz Kostka'
+        author_email = 'lukasz.kostka@netng.pl'
+        base_url = 'napalm-ros'
+        required_settings = []
+        default_settings = {
+        }
+
+    config = NapalmRosConfig
